d2278f3e8d8c480717e17c9eb9c09824d8a9ea37
hojiakbar <hojiakbarni2006@gmail.com>
2024-02-23 23:33:17 +0500

    fixed

--- function.py
+++ function.py
@@ -5,28 +5,30 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, 
 from database import Database
 
 
-def main_menu(context, chat_id):
-    context.bot.send_message(
+async def main_menu(context, chat_id):
+    await context.bot.send_message(
         chat_id=chat_id,
         text="Main menu",
         reply_markup=ReplyKeyboardMarkup(
             [
-                [KeyboardButton(text="Namoz vaqtlari"), KeyboardButton(text="Ma'lumot")]
+                [KeyboardButton(text="Namoz vaqtlari"),
+                 KeyboardButton(text="Ma'lumot")]
             ],
             resize_keyboard=True,
         )
     )
 
 
-def remove_inline(query, context, chat_id, message_id):
-    query.message.edit_text(
+async def remove_inline(query, context, chat_id, message_id):
+    await query.message.edit_text(
         text="🕓",
         reply_markup=None,
     )
-    context.bot.delete_message(chat_id=query.message.chat_id, message_id=message_id)
+    await context.bot.delete_message(
+        chat_id=query.message.chat_id, message_id=message_id)
 
 
-def send_regions(context, chat_id, message_id=None):
+async def send_regions(context, chat_id, message_id=None):
     regions = Database("data.db").get_regions()
     buttons = [[InlineKeyboardButton(text=regions[0]['region_name'], callback_data=f"region_{regions[0]['id']}"),
                 InlineKeyboardButton(text=regions[1]['region_name'], callback_data=f"region_{regions[1]['id']}")],
@@ -40,25 +42,26 @@ def send_regions(context, chat_id, message_id=None):
                 InlineKeyboardButton(text=regions[9]['region_name'], callback_data=f"region_{regions[9]['id']}")],
                [InlineKeyboardButton(text=regions[10]['region_name'], callback_data=f"region_{regions[10]['id']}"),
                 InlineKeyboardButton(text=regions[11]['region_name'], callback_data=f"region_{regions[11]['id']}")],
-               [InlineKeyboardButton(text=regions[12]['region_name'], callback_data=f"region_{regions[12]['id']}")],
+               [InlineKeyboardButton(text=regions[12]['region_name'], callback_data=f"region_{
+                                     regions[12]['id']}")],
                [InlineKeyboardButton(text="Main menu", callback_data="main_menu")]]
 
     if message_id:
-        context.bot.edit_message_text(
+        await context.bot.edit_message_text(
             chat_id=chat_id,
             text="Viloyatlardan birini tanlang",
             reply_markup=InlineKeyboardMarkup(buttons),
             message_id=message_id
         )
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=chat_id,
             text="Viloyatlardan birini tanlang",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
 
 
-def send_photo(context, chat_id, data_sp):
+async def send_photo(context, chat_id, data_sp):
     re = requests.get(f"https://namozvaqti.uz/shahar/{data_sp[1]}")
     soup = BeautifulSoup(re.text, "html.parser")
     city = Database("data.db").get_one_city(data_sp[1])
@@ -66,17 +69,17 @@ def send_photo(context, chat_id, data_sp):
     name = soup.find_all(class_="nam")
     text = soup.find(class_="vil").text.split("            ")
     text_2 = text[1].split("\n")
-    context.bot.sendPhoto(
+    await context.bot.sendPhoto(
         photo=open("mosque.jpg", "rb"),
         chat_id=chat_id,
         caption=f"<strong>🕌{city[0]['city_name']} Namoz vaqtlari</strong>\n\n"
-                f"{name[0].text}:\t\t{time[0].text}  (gacha saharlik)\n"
-                f"{name[1].text}:\t\t{time[1].text}\n"
-                f"{name[2].text}:\t\t{time[2].text}\n"
-                f"{name[3].text}:\t\t{time[3].text}\n"
-                f"{name[4].text}:\t\t{time[4].text}  (dan song iftor)\n"
-                f"{name[5].text}:\t\t{time[5].text}\n\n"
-                f"{text_2[0]}",
+        f"{name[0].text}:\t\t{time[0].text}  (gacha saharlik)\n"
+        f"{name[1].text}:\t\t{time[1].text}\n"
+        f"{name[2].text}:\t\t{time[2].text}\n"
+        f"{name[3].text}:\t\t{time[3].text}\n"
+        f"{name[4].text}:\t\t{time[4].text}  (dan song iftor)\n"
+        f"{name[5].text}:\t\t{time[5].text}\n\n"
+        f"{text_2[0]}",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -91,15 +94,17 @@ def send_photo(context, chat_id, data_sp):
     )
 
 
-def send_cities(context, chat_id, data_sp, message_id=None):
+async def send_cities(context, chat_id, data_sp, message_id=None):
     city = Database("data.db").get_city(region_id=int(data_sp[1]))
     buttons = []
     for i in city:
-        buttons.append([InlineKeyboardButton(text=i['city_name'], callback_data=f"city_{i['path']}")])
-    buttons.append([InlineKeyboardButton(text="Back", callback_data="region_back")])
+        buttons.append([InlineKeyboardButton(
+            text=i['city_name'], callback_data=f"city_{i['path']}")])
+    buttons.append([InlineKeyboardButton(
+        text="Back", callback_data="region_back")])
 
     if message_id:
-        context.bot.edit_message_text(
+        await context.bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
             text="Shahringizni tanlang",
@@ -107,46 +112,53 @@ def send_cities(context, chat_id, data_sp, message_id=None):
         )
 
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=chat_id,
             text="Shahringizni tanlang",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
 
 
-def send_buttons(context, chat_id, message_id=None):
+async def send_buttons(context, chat_id, message_id=None):
     informs = Database("data.db").get_namaz()
     buttons = [
         [
-            InlineKeyboardButton(text=informs[0]['namaz_name'], callback_data=f"namaz_{informs[0]['id']}"),
-            InlineKeyboardButton(text=informs[1]['namaz_name'], callback_data=f"namaz_{informs[1]['id']}")
+            InlineKeyboardButton(text=informs[0]['namaz_name'], callback_data=f"namaz_{
+                                 informs[0]['id']}"),
+            InlineKeyboardButton(text=informs[1]['namaz_name'], callback_data=f"namaz_{
+                                 informs[1]['id']}")
         ],
         [
-            InlineKeyboardButton(text=informs[2]['namaz_name'], callback_data=f"namaz_{informs[2]['id']}"),
-            InlineKeyboardButton(text=informs[3]['namaz_name'], callback_data=f"namaz_{informs[3]['id']}")
+            InlineKeyboardButton(text=informs[2]['namaz_name'], callback_data=f"namaz_{
+                                 informs[2]['id']}"),
+            InlineKeyboardButton(text=informs[3]['namaz_name'], callback_data=f"namaz_{
+                                 informs[3]['id']}")
         ],
         [
-            InlineKeyboardButton(text=informs[4]['namaz_name'], callback_data=f"namaz_{informs[4]['id']}"),
-            InlineKeyboardButton(text=informs[5]['namaz_name'], callback_data=f"namaz_{informs[5]['id']}")
+            InlineKeyboardButton(text=informs[4]['namaz_name'], callback_data=f"namaz_{
+                                 informs[4]['id']}"),
+            InlineKeyboardButton(text=informs[5]['namaz_name'], callback_data=f"namaz_{
+                                 informs[5]['id']}")
         ],
         [InlineKeyboardButton(text="Back", callback_data="main_menu")]
     ]
 
     if message_id:
-        context.bot.edit_message_text(
+        await context.bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
             text="Qaysi namoz haqida bilmoqchisiz ?",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=chat_id,
             text="Qaysi namoz haqida bilmoqchisiz ?",
             reply_markup=InlineKeyboardMarkup(buttons)
         )
 
-def send_namaz_inform(context, chat_id, id):
+
+async def send_namaz_inform(context, chat_id, id):
     inform = Database("data.db").get_one_namaz(id)
     text = (f"{inform[0]['namaz_name']} namozi\n\n"
             f"{inform[0]['text']}")
@@ -154,7 +166,7 @@ def send_namaz_inform(context, chat_id, id):
         [InlineKeyboardButton(text="Back", callback_data="namaz_back")]
     ]
 
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=chat_id,
         reply_markup=InlineKeyboardMarkup(buttons),
         text=text
--- inline_message_handler.py
+++ inline_message_handler.py
@@ -1,35 +1,35 @@
 from function import send_regions, send_photo, send_cities, remove_inline, main_menu, send_buttons, send_namaz_inform
 
 
-def inline_message_handler(update, context):
+async def inline_message_handler(update, context):
     query = update.callback_query
     data_sp = query.data.split("_")
     chat_id = query.message.chat_id
     message_id = query.message.message_id
     if data_sp[0] == "region":
         if data_sp[1] == "back":
-            send_regions(context, chat_id, message_id)
+            await send_regions(context, chat_id, message_id)
         else:
-            send_cities(context, chat_id, data_sp, message_id)
+            await send_cities(context, chat_id, data_sp, message_id)
 
     elif data_sp[0] == "city":
         if data_sp[1] == "Back to region":
-            send_regions(context, chat_id, message_id)
+            await send_regions(context, chat_id, message_id)
         else:
-            send_photo(context, chat_id, data_sp)
-            query.message.edit_text(
+            await send_photo(context, chat_id, data_sp)
+            await query.message.edit_text(
                 text="🕓",
                 reply_markup=None,
             )
-            context.bot.delete_message(chat_id, message_id)
+            await context.bot.delete_message(chat_id, message_id)
 
     elif data_sp[0] == "namaz":
         if data_sp[1] == "back":
-            send_buttons(context, chat_id, message_id)
+            await send_buttons(context, chat_id, message_id)
         else:
-            remove_inline(query, context, chat_id, message_id)
-            send_namaz_inform(context, chat_id, id = data_sp[1])
+            await remove_inline(query, context, chat_id, message_id)
+            await send_namaz_inform(context, chat_id, id=data_sp[1])
 
     elif query.data == "main_menu":
-        remove_inline(query, context, chat_id, message_id)
-        main_menu(context, chat_id)
+        await remove_inline(query, context, chat_id, message_id)
+        await main_menu(context, chat_id)
